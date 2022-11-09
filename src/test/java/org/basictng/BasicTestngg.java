97199181d75a36957957df4608b9c63e58a64e20
ramjic06 <ramjic.06@gmail.com>
2022-11-09 22:00:55 +0530

    ramji new code

---
+++ src/test/java/org/basictng/BasicTestngg.java
@@ -0,0 +1,19 @@
+package org.basictng;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class BasicTestngg {
+
+	@BeforeClass
+	private void beforeClass() {
+		System.out.println("BeforeClass");
+	}
+	
+	@Test
+	private void test() {
+		System.out.println("test");
+	}
+	
+	
+}
